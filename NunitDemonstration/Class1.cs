e6d74fa913a822154165720ea45f0f0f13c180a3
sachinkallukadavilreji <sachinkr690@gmail.com>
2024-02-20 19:01:10 -0500

    Add project files.

---
+++ NunitDemonstration/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Sachin Kalluakdavil Reji Mutation testing
+
+namespace NunitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 110)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
---
+++ TestClass/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NunitDemonstration;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace TestClass
+{
+    public class TriangleTest
+    {
+        [TestFixture]
+
+        public class TriangleTest1
+        {
+            [Test]
+
+            public void validTriangle() 
+            {
+                //Sachin's Test case
+
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;   
+                int thirdAngle = 60;
+                String expected = "The triangle is valid.";
+
+                //Act
+                String actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);    
+
+                //Assert
+                ClassicAssert.AreEqual(expected, actual);   
+
+            }
+        }
+    }
+}
